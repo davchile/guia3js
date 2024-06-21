f6d17ad33044a5c1cc1c2f473a86673ec5323416
David Paillacheo P <david.paillacheo@ticonsultores.cl>
2024-06-20 23:23:06 -0400

    guia 3 start

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// que se muestra en la consola?
+// function MultiplesParametros(par1, par2, par3)
+// {
+// console.log(par3) //muestra el parámetro definido al invocar la funcion, en este caso el "2"
+// }
+// MultiplesParametros(3, 2, 1)
+// function MultiplesParametros(par1, par2 = 2, par3 = 2)
+// {
+// console.log(par2 + par3)
+// }
+// MultiplesParametros(3)
+
+//Función con múltiples parametros
+// function agregarBorde(id, color, width) { //declaro 3 parametros en memoria
+//     const element = document.querySelector(id) //registro el cambio en id
+//     element.style.border = width + 'px solid ' + color //utilizo un espacio para concatenar variables con este método
+// }
+//*border: 1px solid #000 *// sintaxis de css
+//
+// agregarBorde("#imagen-1", "red", 5) //llamo a la funcion e ingreso el valor de los 3 parámetros
+// agregarBorde("#imagen-2", "blue", 3)
+// agregarBorde("#imagen-3", "green", 5)
